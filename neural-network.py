49392c75d020e1ce62de78d8531a5903a9ed1aa7
Harmit Goswami <90732381+harmitgoswami@users.noreply.github.com>
2023-07-11 22:53:48 -0400

    removed example comment

--- neural-network.py
+++ neural-network.py
@@ -20,27 +20,6 @@
 # Image size: 256 x 256
 # HSV colour space
 
-# Example:
-
-# class LargeNet(nn.Module):
-#     def __init__(self):
-#         super(LargeNet, self).__init__()
-#         self.name = "large"
-#         self.conv1 = nn.Conv2d(3, 5, 5)
-#         self.pool = nn.MaxPool2d(2, 2)
-#         self.conv2 = nn.Conv2d(5, 10, 5)
-#         self.fc1 = nn.Linear(10 * 5 * 5, 32)
-#         self.fc2 = nn.Linear(32, 1)
-
-#     def forward(self, x):
-#         x = self.pool(F.relu(self.conv1(x)))
-#         x = self.pool(F.relu(self.conv2(x)))
-#         x = x.view(-1, 10 * 5 * 5)
-#         x = F.relu(self.fc1(x))
-#         x = self.fc2(x)
-#         x = x.squeeze(1) # Flatten to [batch_size]
-#         return x
-
 class FruitRipenessDetector(nn.Module):
     def __init__(self):
          super(FruitRipenessDetector, self).__init__()
